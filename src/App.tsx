e6ccec7f2f4cbac5ee9886ce3d4337a94bd12e55
Bryan Joseph <bryanllana14@gmail.com>
2024-03-21 13:29:33 -0500

    List activities added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer } from "react"
 import Form from "./components/Form"
 import Header from "./components/Header"
 import { activityReducer, initialState } from "./reducers/activity.reducer"
+import ActivityList from "./components/ActivityList"
 
 const App = () => {
   const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -14,6 +15,10 @@ const App = () => {
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} />
         </div>
+      </section>
+
+      <section className="p-10 mx-auto max-w-4xl px-5 md:px-0">
+        <ActivityList activities={state.activites} />
       </section>
     </>
   )
